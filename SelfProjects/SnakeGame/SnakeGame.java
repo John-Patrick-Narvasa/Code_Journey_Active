42d3aa71767b53dc508a47be4263936bf36ab890
John-Patrick-Narvasa <narvasajohnpatrick@gmail.com>
2025-02-17 01:36:09 +0800

    SnakrGameProj

---
+++ SelfProjects/SnakeGame/SnakeGame.java
@@ -0,0 +1,11 @@
+package SnakeGame;
+//
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+       GameFrame frame = new GameFrame();
+        // shortcut ver: new GameFrame();
+    }
+      
+}
